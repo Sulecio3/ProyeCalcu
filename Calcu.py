db07b7ffde465d5c95430747a0f8dafcd93fb658
Xantonio30111 <114781696+Xantonio30111@users.noreply.github.com>
2025-07-17 07:43:38 -0600

    Avance del proyecto 1

--- Calcu.py
+++ Calcu.py
@@ -1,1 +1,22 @@
 print("Bienvenido al programa de Calculadora")
+opcion = ""
+
+while opcion != "5":
+    print("\n--- Calculadora Básica ---")
+    print("1. Sumar")
+    print("2. Restar")
+    print("3. Multiplicar")
+    print("4. Dividir")
+    print("5. Salir")
+
+    opcion = input("Elige una opción (1-5): ")
+
+    if opcion == "1":
+        a = float(input("Ingresa el primer número: "))
+        b = float(input("Ingresa el segundo número: "))
+        print("Resultado:", a + b)
+
+    elif opcion == "2":
+        a = float(input("Ingresa el primer número: "))
+        b = float(input("Ingresa el segundo número: "))
+        print("Resultado:", a - b)
